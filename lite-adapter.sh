dd3ea6ebe4c5349e908551a5576f27f005a3a8e6
Pierre-Hugues Husson <phh@phh.me>
2020-09-20 21:42:11 +0200

    Add lite-adapter that adds some libs used on vndk lite devices to vndk

---
+++ lite-adapter.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#cleanups
+umount d
+
+set -ex
+
+origin="$(readlink -f -- "$0")"
+origin="$(dirname "$origin")"
+
+[ ! -d vendor_vndk ] && git clone https://github.com/phhusson/vendor_vndk
+
+simg2img /build2/AOSP-11.0/out/target/product/phhgsi_arm64_ab/system.img s.img
+rm -Rf tmp
+mkdir -p d tmp
+e2fsck -f s.img
+resize2fs s.img 2500M
+mount -o loop,rw s.img d
+(
+cd d
+for vndk in 28;do
+    for arch in 32 64;do
+        for lib in $(cd "$origin/vendor_vndk/vndk-${vndk}-arm${arch}"; echo *);do
+            p=lib
+            [ "$arch" = 64 ] && p=lib64
+            cp "$origin/vendor_vndk/vndk-${vndk}-arm${arch}/$lib" system/system_ext/apex/com.android.vndk.v${vndk}/${p}/$lib
+            xattr -w security.selinux u:object_r:system_lib_file:s0 system/system_ext/apex/com.android.vndk.v${vndk}/${p}/$lib
+            echo $lib >> system/system_ext/apex/com.android.vndk.v${vndk}/etc/vndkcore.libraries.${vndk}.txt
+        done
+        sort -u system/system_ext/apex/com.android.vndk.v${vndk}/etc/vndkcore.libraries.${vndk}.txt > v
+        mv -f v system/system_ext/apex/com.android.vndk.v${vndk}/etc/vndkcore.libraries.${vndk}.txt
+        xattr -w security.selinux u:object_r:system_file:s0 system/system_ext/apex/com.android.vndk.v${vndk}/etc/vndkcore.libraries.${vndk}.txt
+    done
+done
+
+
+)
+sleep 1
+
+
+umount d
+
+e2fsck -f -y s.img || true
+resize2fs -M s.img
